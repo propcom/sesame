6f46a7fe6208355e736fd651ac5f19a5ca9e2771
Alastair McGowan-Douglas <alastair@propcom.co.uk>
2013-03-13 15:36:57 +0000

    ACL can be a root class too why not

--- bootstrap.php
+++ bootstrap.php
@@ -2,6 +2,7 @@
 
 \Autoloader::add_namespace( 'Sesame', __DIR__ . '/classes/' );
 \Autoloader::alias_to_namespace('Sesame\Sesame');
+\Autoloader::alias_to_namespace('Sesame\ACL');
 
 $module_paths = \Config::get('module_paths');
 $module_paths[] = __DIR__ . '/modules/';
